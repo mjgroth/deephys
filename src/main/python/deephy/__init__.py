cfba6782706cca6af6ada3e351a3f12c9beec885
mgroth0 <mgroth49@gmail.com>
2022-09-26 16:13:09 -0400

    autocommit

--- src/main/python/deephy/__init__.py
+++ src/main/python/deephy/__init__.py
@@ -1,2 +1,19 @@
-from deephy.deephy import ImageFile, Neuron, Layer, Model, Test
-__all__ = ["ImageFile", "Neuron", "Layer", "Model", "Test"]
+from deephy.deephy import (
+    ImageFile,
+    Neuron,
+    Layer,
+    Model,
+    Test,
+    Stopwatch,
+    import_torch_dataset,
+)
+
+__all__ = [
+    "ImageFile",
+    "Neuron",
+    "Layer",
+    "Model",
+    "Test",
+    "Stopwatch",
+    "import_torch_dataset",
+]
