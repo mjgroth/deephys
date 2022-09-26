3421da47c43393b4957d55d106c67e9f14aa007f
mgroth0 <mgroth49@gmail.com>
2022-09-25 21:39:26 -0400

    autocommit

--- src/main/kotlin/matt/nn/deephy/load/test/test.kt
+++ src/main/kotlin/matt/nn/deephy/load/test/test.kt
@@ -2,11 +2,14 @@ package matt.nn.deephy.load.test
 
 import matt.async.thread.daemon
 import matt.cbor.CborItemReader
+import matt.cbor.err.CborParseException
 import matt.cbor.read.major.array.ArrayReader
+import matt.cbor.read.major.bytestr.ByteStringReader
 import matt.cbor.read.major.map.MapReader
 import matt.cbor.read.streamman.cborReader
 import matt.cbor.read.streamman.readCbor
 import matt.file.CborFile
+import matt.lang.List2D
 import matt.lang.sync
 import matt.log.profile.tic
 import matt.log.warn
@@ -109,39 +112,38 @@ class TestLoader(
 		t.toc("got stream")
 		try {
 		  val reader = stream.cborReader()
-		  reader.readManually<MapReader> {
+		  reader.readManually<MapReader, Unit> {
 			if (count != 3.toULong()) {
 			  warn("expected 3 name-value pairs but got $count")
 			  signalParseError()
 			  return@daemon
 			}
 			val name = nextValue<String>(requireKeyIs = "name")
-			val suffix = nextValue<String>(requireKeyIs = "suffix")
+			val suffix = nextValue<String?>(requireKeyIs = "suffix")
 
-			nextValueManual<ArrayReader>(requireKeyIs = "images") {
+			nextValueManual<ArrayReader, Unit>(requireKeyIs = "images") {
 			  numImages.putLoadedValue(count)
 			  println("numImages=$count")
 
-			  readEachManually<MapReader> {
-				val imageID = nextValue<String>(requireKeyIs = "imageID")
-				val categoryID = nextValue<String>(requireKeyIs = "categoryID")
+			  readEachManually<MapReader, Unit> {
+				val imageID = nextValue<Int>(requireKeyIs = "imageID")
+				val categoryID = nextValue<Int>(requireKeyIs = "categoryID")
 				val category = nextValue<String>(requireKeyIs = "category")
 				val imageData: Any = if (numDataBytes == null) {
 				  var willBeNumHeaderBytes = 0
 
-				  nextValueManual<ArrayReader>(requireKeyIs = "data") {
+				  nextValueManual<ArrayReader, List2D<IntArray>>(requireKeyIs = "data") {
 					willBeNumHeaderBytes += head.numBytes
-					require(hasCount) /*make sure there is a cunt*/
+					require(hasCount) /*make sure there is a count*/
 
-					val r = dataReader.mapElements {
+					val r = readEachManually<ArrayReader, List<IntArray>> {
 					  willBeNumHeaderBytes += head.numBytes
-					  require(it.length != null)
-					  it.mapElements {
-						require(length != null)
-						it as ByteArray
-						willBeNumHeaderBytes += numCborBytesFor(it)
-						val r = IntArray(it.size)
-						for ((i, b) in it.withIndex()) {
+					  require(hasCount)
+					  readEachManually<ByteStringReader, IntArray> {
+						require(hasCount)
+						willBeNumHeaderBytes += head.numBytes + count.toInt()
+						val r = IntArray(count.toInt())
+						for ((i, b) in read().raw.withIndex()) {
 						  r[i] = b.toInt() and 0xff
 						}
 						r
@@ -150,37 +152,38 @@ class TestLoader(
 					numDataBytes = willBeNumHeaderBytes
 					r
 				  }
-
-
 				} else {
-				  val key = reader.next()
-				  require(key == "data")
-				  reader.stream.readNBytes(numDataBytes!!)
+				  require(nextKeyOnly<String>() == "data")
+				  stream.readNBytes(numDataBytes!!)
 				}
-				val modelReader = it.nextValue(requireKeyIs = "activations") as CborMapReader
-				val activationsThing: Any = if (numActivationBytes == null) {
-				  var willBeNumHeaderBytes = 0
-				  val activationsReader = modelReader.nextValue(requireKeyIs = "activations") as CborArrayReader
-				  willBeNumHeaderBytes += activationsReader.numHeaderBytes
-				  val activations = activationsReader.mapElements {
-					it as ByteArray
-					willBeNumHeaderBytes += numCborBytesFor(it)
-					val r = FloatArray(it.size)
-					ByteBuffer.wrap(it).asFloatBuffer().get(r)
-					r
+
+				val activationsThing: Any = nextValueManual<MapReader, Any>(requireKeyIs = "activations") {
+				  if (numActivationBytes == null) {
+					var willBeNumHeaderBytes = 0
+					nextValueManual<ArrayReader, List<FloatArray>>(requireKeyIs = "activations") {
+					  willBeNumHeaderBytes += head.numBytes
+					  val activations = readEachManually<ByteStringReader, FloatArray> {
+						willBeNumHeaderBytes += head.numBytes + count.toInt()
+						val r = FloatArray(count.toInt())
+						ByteBuffer.wrap(read().raw.toByteArray()).asFloatBuffer().get(r)
+						r
+					  }
+					  numActivationBytes = willBeNumHeaderBytes
+					  println("numActivationBytes=$numActivationBytes")
+					  activations
+					}
+				  } else {
+					require(nextKeyOnly<String>() == "activations")
+					stream.readNBytes(numActivationBytes!!)
 				  }
-				  numActivationBytes = willBeNumHeaderBytes
-				  println("numActivationBytes=$numActivationBytes")
-				  activations
-				} else {
-				  val key = reader.next()
-				  require(key == "activations")
-				  reader.stream.readNBytes(numActivationBytes!!)
 				}
 
-				@Suppress("KotlinConstantConditions", "UNCHECKED_CAST")
+
+				@Suppress("UNCHECKED_CAST")
 				val deephyImage = DeephyImage(
-				  imageID = imageID as Int, categoryID = categoryID as Int, category = category as String,
+				  imageID = imageID,
+				  categoryID = categoryID,
+				  category = category,
 				  activations = ModelState().apply {
 					when (activationsThing) {
 					  is List<*> -> activations.putLoadedValue(activationsThing as List<FloatArray>)
@@ -188,17 +191,17 @@ class TestLoader(
 						dataLoaderDispatcher.execute {
 						  loadJobStartedCount += 1
 						  val workerReader = (activationsThing as ByteArray).inputStream().buffered()
-						  val workerCborReader = CborItemReader(workerReader)
-						  val actsReader = workerCborReader.next() as CborArrayReader
-						  require(actsReader.length != null)
-						  val theData = actsReader.mapElements {
-							it as ByteArray
-							val r = FloatArray(it.size)
-							ByteBuffer.wrap(it).asFloatBuffer().get(r)
-							r
+						  val workerCborReader = workerReader.cborReader()
+						  workerCborReader.readManually<ArrayReader, Unit> {
+							require(hasCount)
+							val theData = readEachManually<ByteStringReader, FloatArray> {
+							  val r = FloatArray(count.toInt())
+							  ByteBuffer.wrap(read().raw.toByteArray()).asFloatBuffer().get(r)
+							  r
+							}
+							activations.putLoadedValue(theData)
+							loadJobFinishedCount += 1
 						  }
-						  activations.putLoadedValue(theData)
-						  loadJobFinishedCount += 1
 						}
 					  }
 					}
@@ -212,23 +215,22 @@ class TestLoader(
 					  dataLoaderDispatcher.execute {
 						loadJobStartedCount += 1
 						val workerReader = (imageData as ByteArray).inputStream().buffered()
-						val workerCborReader = CborItemReader(workerReader)
-						val dataReader = workerCborReader.next() as CborArrayReader
-						require(dataReader.length != null)
-						val theData = dataReader.mapElements {
-						  it as CborArrayReader
-						  require(it.length != null)
-						  it.mapElements {
-							it as ByteArray
-							val r = IntArray(it.size)
-							for ((i, b) in it.withIndex()) {
-							  r[i] = b.toInt() and 0xff
+						workerReader.cborReader().readManually<ArrayReader, Unit> {
+						  require(hasCount)
+						  val theData = readEachManually<ArrayReader, List<IntArray>> {
+							require(hasCount)
+							readEachManually<ByteStringReader, IntArray> {
+							  val r = IntArray(count.toInt())
+							  for ((i, b) in read().raw.withIndex()) {
+								r[i] = b.toInt() and 0xff
+							  }
+							  r
 							}
-							r
 						  }
+						  data.putLoadedValue(theData)
+						  loadJobFinishedCount += 1
 						}
-						data.putLoadedValue(theData)
-						loadJobFinishedCount += 1
+
 					  }
 					  imageData as ByteArray
 					}
@@ -243,7 +245,7 @@ class TestLoader(
 			  }
 			}
 			finishedTest = Test(
-			  name = name as String, suffix = suffix as String?, images = finishedImages
+			  name = name, suffix = suffix, images = finishedImages
 			).apply {
 			  model = this@TestLoader.model
 			}
