fe90059e3c0232d98350d087dc0f6b80fde8a5d3
mgroth0 <mgroth49@gmail.com>
2022-12-22 03:28:24 -0500

    autocommit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ extensions = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'torch': (https://pytorch.org/docs/master,None)
+    'torch': ('https://pytorch.org/docs/master',None)
 }
 intersphinx_disabled_domains = ['std']
 
