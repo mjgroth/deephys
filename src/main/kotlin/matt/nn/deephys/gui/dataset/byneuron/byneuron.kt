c2e889dc719d1e041f45d1b8877b60b73a0bc430
mgroth0 <mgroth49@gmail.com>
2023-01-25 08:40:54 -0500

    autocommit

--- src/main/kotlin/matt/nn/deephys/gui/dataset/byneuron/byneuron.kt
+++ src/main/kotlin/matt/nn/deephys/gui/dataset/byneuron/byneuron.kt
@@ -3,11 +3,11 @@ package matt.nn.deephys.gui.dataset.byneuron
 import matt.fx.graphics.wrapper.pane.anchor.swapper.Swapper
 import matt.nn.deephys.gui.layer.LayerView
 import matt.nn.deephys.gui.viewer.DatasetViewer
-import matt.nn.deephys.load.test.TestLoader
 import matt.nn.deephys.model.ResolvedLayer
+import matt.nn.deephys.model.importformat.testlike.TypedTestLike
 
 class ByNeuronView(
-  testLoader: TestLoader, viewer: DatasetViewer
+  testLoader: TypedTestLike<*>, viewer: DatasetViewer
 ): Swapper<ResolvedLayer?, LayerView>() {
   init {
 	setupSwapping(
