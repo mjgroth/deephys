0ca909bc1aff0e5292bd0925fb6f5cbcdc4db674
mgroth0 <mgroth49@gmail.com>
2023-07-14 10:39:14 -0400

    changed src/main/kotlin/matt/nn/deephys/gui/global/color/color.kt

--- src/main/kotlin/matt/nn/deephys/gui/global/color/color.kt
+++ src/main/kotlin/matt/nn/deephys/gui/global/color/color.kt
@@ -1,46 +1,46 @@
 package matt.nn.deephys.gui.global.color
 
 import javafx.scene.paint.CycleMethod.NO_CYCLE
-import matt.color.hexToAwtColor
+import matt.color.hexToColor
 import matt.fx.graphics.style.DarkModeController
 import matt.fx.graphics.wrapper.style.gradient.linearGradient
 import matt.fx.graphics.wrapper.style.toFXColor
 import matt.obs.bind.binding
 
 object DeephysPalette {
-  val deephysBlue1 = hexToAwtColor("#00bbe2").toFXColor()
-  val deephysBlue2 = hexToAwtColor("#3360ad").toFXColor()
-  val deephysRed1 = hexToAwtColor("#f5c39e").toFXColor()
-  val deephysRed2 = hexToAwtColor("#da1d52").toFXColor()
-  val deephysSelectGradient by lazy {
-	linearGradient {
-	  startX = 0.0
-	  startY = 0.5
-	  endX = 1.0
-	  endY = 0.5
-	  cycleMethod = NO_CYCLE
-	  stop(
-		0.0,
-		/*Color.YELLOW.deriveColor(0.0, 1.0, 1.0, 0.5)*/
-		deephysBlue1
-	  )
-	  stop(
-		1.0,
-		/*Color.TRANSPARENT*/
-		deephysBlue2
-	  )
-	}
-  }
+    val deephysBlue1 = hexToColor("#00bbe2").toFXColor()
+    val deephysBlue2 = hexToColor("#3360ad").toFXColor()
+    val deephysRed1 = hexToColor("#f5c39e").toFXColor()
+    val deephysRed2 = hexToColor("#da1d52").toFXColor()
+    val deephysSelectGradient by lazy {
+        linearGradient {
+            startX = 0.0
+            startY = 0.5
+            endX = 1.0
+            endY = 0.5
+            cycleMethod = NO_CYCLE
+            stop(
+                0.0,
+                /*Color.YELLOW.deriveColor(0.0, 1.0, 1.0, 0.5)*/
+                deephysBlue1
+            )
+            stop(
+                1.0,
+                /*Color.TRANSPARENT*/
+                deephysBlue2
+            )
+        }
+    }
 
-  val tooltipBackground by lazy {
-	DarkModeController.darkModeProp.binding {
-	  if (it) {
-		hexToAwtColor("#111111").toFXColor()
-	  } else {
-		hexToAwtColor("#eeeeee").toFXColor()
-	  }
-	}
-  }
+    val tooltipBackground by lazy {
+        DarkModeController.darkModeProp.binding {
+            if (it) {
+                hexToColor("#111111").toFXColor()
+            } else {
+                hexToColor("#eeeeee").toFXColor()
+            }
+        }
+    }
 
 }
 
